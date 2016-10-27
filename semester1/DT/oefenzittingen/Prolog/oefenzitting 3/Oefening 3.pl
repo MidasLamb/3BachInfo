a9c1b043cf0c29927a6f6faf2227a9c291f7ced7
Midas Lambrichts <midas_lambrichts@hotmail.com>
2016-10-27 13:02:04 +0200

    Added my solution

--- semester1/DT/oefenzittingen/Prolog/oefenzitting 3/Oefening 3.pl
+++ semester1/DT/oefenzittingen/Prolog/oefenzitting 3/Oefening 3.pl
@@ -23,3 +23,16 @@ read_tape(pair(C,_),C).
 
 % write/3: Writes a symbol under the head.
 write_tape(C,pair(_,R),pair(C,R)).
+
+%% Other solution which exists out of representing the tape as 1 pair of lists, where the first element of the "right" list represents the element under the head:
+% move/3: Moves the head in a certain direction.
+move(right,pair([L|Lr],[R|Rr]),pair([R,L|Lr],[Rr])).
+
+    
+move(left,pair([L|Lr],[R|Rr]),pair([Lr],[L,R|Rr])).
+
+% read/2: Reads the symbol under the head.
+read_tape(pair([_],[R|_],R)).
+
+% write/3: Writes a symbol under the head.
+write_tape(S,pair([_],[_|_]),pair([_],[S|_])).
