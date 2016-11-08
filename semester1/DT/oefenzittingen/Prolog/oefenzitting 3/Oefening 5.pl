d55337b191375a7284e7234ce143c8a2e9d84d14
HannePeeters <HannePeeters@users.noreply.github.com>
2016-11-08 22:13:35 +0100

    Removed simular solution

--- semester1/DT/oefenzittingen/Prolog/oefenzitting 3/Oefening 5.pl
+++ semester1/DT/oefenzittingen/Prolog/oefenzitting 3/Oefening 5.pl
@@ -1,36 +1,3 @@
-queens(N,PList) :-
-    numlist(1,N,List),
-    permutation(List,PList),
-    validConfiguration(N,PList).
-    
-    
-validConfiguration(N,Configuration) :-
-    checkDiagonaal(N,Configuration,1).
-    
-checkDiagonaal(N,_,Column) :-
-    N =< Column , !.
-checkDiagonaal(N,[Position|Rest],Column) :-
-    N > Column,
-    checkDiagonaalForPosition(N,Position,Rest,Column,1),
-    ColumnNew is Column + 1,
-    checkDiagonaal(N,Rest,ColumnNew).
-    
-checkDiagonaalForPosition(N,_,_,Column,Offset) :-
-    N < Column + Offset, !.
-    
-checkDiagonaalForPosition(N,Position,[Head|Tail],Column,Offset) :-
-    N >= Column + Offset,
-    
-    PositionUp is Position + Offset,
-    PositionDown is Position - Offset,
-    
-    PositionUp \= Head,
-    PositionDown \= Head,
-    
-    OffsetNew is Offset + 1,
-    checkDiagonaalForPosition(N,Position,Tail,Column,OffsetNew).
-    
-%Other solution
 queens(N,L) :-
     numlist(1,N,Lijst),
     permutation(L,Lijst),
