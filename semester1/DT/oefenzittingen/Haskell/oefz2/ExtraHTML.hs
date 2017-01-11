2119fe51f12f54803553de337fbd8f1987e965ae
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-01-11 15:25:26 +0100

    HTML

--- semester1/DT/oefenzittingen/Haskell/oefz2/ExtraHTML.hs
+++ semester1/DT/oefenzittingen/Haskell/oefz2/ExtraHTML.hs
@@ -25,10 +25,10 @@ data Link =
   deriving (Eq,Show)
 
 instance HTML Link where
-  toHtml = error "Not implemented"
+  toHtml (Link link text) = HtmlTag "a" [Attr "href" link] [HtmlString text]
 
 instance HTML a => HTML [a] where
-  toHtml = error "Not implemented"
+  toHtml xs = HtmlTag "ul" [] [toHtml x | x <- xs]
 
 -- The encoding of the following unordered list as an HtmlElement
 --   <ul>
@@ -37,15 +37,23 @@ instance HTML a => HTML [a] where
 --   <li>Oranges</li>
 --   </ul>
 exampleUL :: HtmlElement
-exampleUL = error "Not implemented"
+exampleUL = HtmlTag "ul" [] [HtmlTag "li" [] [HtmlString "Appels"],HtmlTag "li" [] [HtmlString "Bananas"],HtmlTag "li" [] [HtmlString "Oranges"]]
 
-data AddressBook = DEFINE_ADDRESSBOOK
+data AddressBook = Book [Adress]
+data Adress = Adress String String [Email]
+data Email = Work String | Private String
 
 myAddressBook :: AddressBook
-myAddressBook = error "Not implemented"
+myAddressBook = Book [Adress "Yana" "Berbers" [Work "yana.berbers@kul.be"], Adress "Jonah" "Moens" [Work "jonah.moens@kul.be"]]
 
 instance HTML AddressBook where
-  toHtml = error "Not implemented"
+  toHtml (Book list) = toHtml list
+instance HTML Adress where
+    toHtml (Adress first last emails) = HtmlTag "section" [] [HtmlTag "h1" [] [HtmlString (first ++ " " ++ last)], toHtml emails]
+instance HTML Email where
+    toHtml x
+        | Work email <- x       = HtmlString email
+        | Private email <- x    = HtmlString email
 
 renderElement :: HtmlElement -> String
 renderElement = error "Not implemented"
