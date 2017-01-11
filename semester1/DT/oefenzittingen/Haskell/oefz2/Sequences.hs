b97e4f7dddb7cafabb2f358c5a2431f43e2b3e85
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-01-11 14:34:12 +0100

    Sequences

--- semester1/DT/oefenzittingen/Haskell/oefz2/Sequences.hs
+++ semester1/DT/oefenzittingen/Haskell/oefz2/Sequences.hs
@@ -1,4 +1,49 @@
+module Template where
 
 -- * Sequences
 -- ----------------------------------------------------------------------------
+
+class Sequence a where
+  prev :: a -> a
+  next :: a -> a
+
+instance Sequence Int where
+  prev x = x - 1
+  next = (+ 1)
+
+instance Sequence Char where
+  prev 'a' = error "no value before 'a'"
+  prev x = reverse ['a'..x] !! 1
+  next 'z' = error "no value after 'z'"
+  next x = [x..'z'] !! 1
+
+
+instance Sequence Bool where
+  prev = not
+  next = not
+
+class Sequence a => LeftBoundedSequence a where
+  firstElem :: a
+
+class Sequence a => RightBoundedSequence a where
+  lastElem :: a
+
+instance LeftBoundedSequence Int where
+  firstElem = minBound :: Int
+
+instance LeftBoundedSequence Char where
+  firstElem = 'a'
+
+instance LeftBoundedSequence Bool where
+  firstElem = minBound :: Bool
+
+instance RightBoundedSequence Int where
+  lastElem = maxBound :: Int
+
+instance RightBoundedSequence Char where
+  lastElem = 'z'
+
+instance RightBoundedSequence Bool where
+  lastElem = maxBound :: Bool
+
 
