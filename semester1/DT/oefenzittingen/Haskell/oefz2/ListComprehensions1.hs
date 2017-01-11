3ca3c94e57071f3d0f38c9243cf33e8f01c4e42f
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-01-11 14:16:44 +0100

    list comp

--- semester1/DT/oefenzittingen/Haskell/oefz2/ListComprehensions1.hs
+++ semester1/DT/oefenzittingen/Haskell/oefz2/ListComprehensions1.hs
@@ -1,6 +1,6 @@
 mapLC :: (a -> b) -> [a] -> [b]
-mapLC = error "Not implemented"
+mapLC f xs = [f x | x <- xs]
 
 filterLC :: (a -> Bool) -> [a] -> [a]
-filterLC = error "Not implemented"
+filterLC f xs = [x | x <- xs, f x]
 
