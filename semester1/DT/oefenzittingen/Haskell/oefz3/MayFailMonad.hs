3059cce081355e2d7cb24486d08fad3c86a56102
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-01-12 14:24:42 +0100

    Mayfail monad

--- semester1/DT/oefenzittingen/Haskell/oefz3/MayFailMonad.hs
+++ semester1/DT/oefenzittingen/Haskell/oefz3/MayFailMonad.hs
@@ -15,22 +15,51 @@ safeDiv a b | b == 0    = Error "Division by zero"
             | otherwise = Result (div a b)
 
 instance Functor (MayFail e) where
-  fmap = error "Not implemented"
+  fmap f (Error e) = Error e
+  fmap f (Result a) = Result (f a)
 
 instance Applicative (MayFail e) where
   pure  = return
   (<*>) = ap
 
 instance Monad (MayFail e) where
-  return = error "Not implemented"
-  (>>=)  = error "Not implemented"
+  return x = Result x
+  (>>=) m f  = case m of
+                    Result a -> f a
+                    Error e  -> Error e
 
 data Exp = Lit Int | Add Exp Exp | Mul Exp Exp | Div Exp Exp
   deriving (Eq, Ord, Show)
 
 eval :: Exp -> MayFail String Int
-eval = error "Not implemented"
-
+eval e
+    |Lit x <- e = return x
+    |Add e1 e2 <- e =   eval e1 >>= \x ->
+                        eval e2 >>= \y ->
+                        return $ x+y
+    |Mul e1 e2 <- e =   eval e1 >>= \x ->
+                        eval e2 >>= \y ->
+                        return $ x*y
+    |Div e1 e2 <- e =   eval e1 >>= \x ->
+                        eval e2 >>= \y ->
+                        if(y == 0)
+                            then Error "Division by zero"
+                            else return $ div x y
 evalDo :: Exp -> MayFail String Int
-evalDo = error "Not implemented"
+evalDo e 
+    |Lit x <- e = return x
+    |Add e1 e2 <- e = do
+                        x <- eval e1
+                        y <- eval e2
+                        return $ x + y
+    |Mul e1 e2 <- e = do
+                        x <- eval e1
+                        y <- eval e2
+                        return $ x * y
+    |Div e1 e2 <- e = do
+                        x <- eval e1
+                        y <- eval e2
+                        if(y == 0)
+                            then Error "Division by zero"
+                            else return $ div x y
 
