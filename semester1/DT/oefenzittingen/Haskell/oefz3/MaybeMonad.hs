976c710d547e298a4c06ede8d24cc5622261f2d4
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-01-12 12:22:14 +0100

    maybe monad

--- semester1/DT/oefenzittingen/Haskell/oefz3/MaybeMonad.hs
+++ semester1/DT/oefenzittingen/Haskell/oefz3/MaybeMonad.hs
@@ -8,11 +8,26 @@ import Control.Monad
 -- ----------------------------------------------------------------------------
 
 sumABC :: [(String, Int)] -> Maybe Int
-sumABC = error "Not implemented"
+sumABC l = 
+    case lookup "A" l of
+        Just a  -> case lookup "B" l of
+                        Just b  -> case lookup "C" l of
+                                        Just c  -> Just (a+b+c)
+                                        Nothing -> Nothing
+                        Nothing -> Nothing
+        Nothing -> Nothing
 
 sumABCBind :: [(String, Int)] -> Maybe Int
-sumABCBind = error "Not implemented"
+sumABCBind l = 
+        lookup "A" l >>= \a ->
+        lookup "B" l >>= \b ->
+        lookup "C" l >>= \c ->
+        Just (a+b+c)
 
 sumABCDo :: [(String, Int)] -> Maybe Int
-sumABCDo = error "Not implemented"
+sumABCDo l = do     
+                a <- lookup "A" l
+                b <- lookup "B" l
+                c <- lookup "C" l
+                Just (a+b+c)
 
