9c2b60d68d87da5e5fe6048ae39549ba906c80d4
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-01-12 14:50:28 +0100

    Writer monad

--- semester1/DT/oefenzittingen/Haskell/oefz3/WriterMonad.hs
+++ semester1/DT/oefenzittingen/Haskell/oefz3/WriterMonad.hs
@@ -11,25 +11,46 @@ data Exp = Lit Int | Add Exp Exp | Mul Exp Exp
   deriving (Eq, Ord, Show)
 
 evalTrace :: Exp -> (Int, String)
-evalTrace = error "Not implemented"
+evalTrace (Lit x) = (x,"Lit\n")
+evalTrace (Add e1 e2) = (x+y,"Add\n" ++ sx ++ sy)
+    where
+        (x,sx) = evalTrace e1
+        (y,sy) = evalTrace e2
+
+evalTrace (Mul e1 e2) = (x*y,"Mul\n" ++ sx ++ sy)
+    where
+        (x,sx) = evalTrace e1
+        (y,sy) = evalTrace e2
+
 
 data Writer a = Writer a String
   deriving (Eq, Ord, Show)
 
 instance Functor Writer where
-  fmap = error "Not implemented"
+  fmap f (Writer a s) = Writer (f a) s
 
 instance Applicative Writer where
   pure  = return
   (<*>) = ap
 
 instance Monad Writer where
-  return = error "Not implemented"
-  (>>=)  = error "Not implemented"
+  return x = Writer x ""
+  Writer x s >>= f = case f x of
+    Writer y s' -> Writer y (s ++ s')
 
 trace :: String -> Writer ()
-trace = error "Not implemented"
+trace s = Writer () s
 
 evalTraceM :: Exp -> Writer Int
-evalTraceM = error "Not implemented"
+evalTraceM (Lit x) = Writer x "Lit\n"
+evalTraceM (Add x y) = do
+  trace "Add\n"
+  vx <- evalTraceM x
+  vy <- evalTraceM y
+  return (vx + vy)
+evalTraceM (Mul x y) = do
+  trace "Mul\n"
+  vx <- evalTraceM x
+  vy <- evalTraceM y
+  return (vx * vy)
 
